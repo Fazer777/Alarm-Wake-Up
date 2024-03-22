e2456e877a0f362aa5d5b3aae2bde8e5956224be
Fazer777 <m.soup2016@yandex.ru>
2024-03-22 16:34:54 +0300

    Added DI for database

--- app/src/main/java/com/project/alarmwakeup/presentation/di/DataModule.kt
+++ app/src/main/java/com/project/alarmwakeup/presentation/di/DataModule.kt
@@ -2,6 +2,8 @@ package com.project.alarmwakeup.presentation.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.project.alarmwakeup.data.alarm_clock.AlarmClockRepositoryImpl
 import com.project.alarmwakeup.data.alarm_clock.IAlarmClockDao
 import com.project.alarmwakeup.data.database.AppDatabase
@@ -11,15 +13,20 @@ import org.koin.dsl.module
 
 
 val dataModule = module {
+
+
+
     single<IAlarmClockRepository>{
         AlarmClockRepositoryImpl(alarmDao = get())
     }
 
     fun providesDao(db : AppDatabase) : IAlarmClockDao = db.alarmDao()
 
-    fun providesDatabase(context : Context): AppDatabase =
-        Room.databaseBuilder(context,AppDatabase::class.java,"Alarms.db")
+    fun providesDatabase(context : Context): AppDatabase {
+        return Room.databaseBuilder(context,AppDatabase::class.java,"Alarms.db")
             .build()
+    }
+
 
     single<IAlarmClockDao> {
         providesDao(get())
