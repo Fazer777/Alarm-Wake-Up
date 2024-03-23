55c6d61d395d51f5930c56aff1eeb50dfeffbb8f
Fazer777 <m.soup2016@yandex.ru>
2024-03-23 19:22:34 +0300

    Created use case - turn on / off alarm clock

---
+++ app/src/main/java/com/project/alarmwakeup/domain/alarm_clock/usecase/SwitchEnablingUseCase.kt
@@ -0,0 +1,9 @@
+package com.project.alarmwakeup.domain.alarm_clock.usecase
+
+import com.project.alarmwakeup.domain.alarm_clock.IAlarmClockRepository
+
+class SwitchEnablingUseCase(private val alarmRepository: IAlarmClockRepository) {
+    suspend fun execute(alarmClockId : Int, isEnabled : Boolean){
+        alarmRepository.updateEnablingAlarmClockDb(alarmClockId = alarmClockId, isEnabled = isEnabled)
+    }
+}
