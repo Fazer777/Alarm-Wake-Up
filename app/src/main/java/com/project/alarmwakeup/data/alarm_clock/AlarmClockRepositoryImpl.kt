a4562512634de8cc0a7e739fa13267f68a764639
Fazer777 <m.soup2016@yandex.ru>
2024-03-26 12:38:18 +0300

    Updating mappers with new type

--- app/src/main/java/com/project/alarmwakeup/data/alarm_clock/AlarmClockRepositoryImpl.kt
+++ app/src/main/java/com/project/alarmwakeup/data/alarm_clock/AlarmClockRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken
 import com.project.alarmwakeup.data.alarm_clock.models.AlarmClock
 import com.project.alarmwakeup.domain.alarm_clock.IAlarmClockRepository
 import com.project.alarmwakeup.domain.alarm_clock.models.AlarmInterim
+import com.project.alarmwakeup.domain.alarm_clock.models.Day
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.lang.reflect.Type
@@ -41,11 +42,12 @@ class AlarmClockRepositoryImpl(private val alarmDao : IAlarmClockDao) : IAlarmCl
         return AlarmClock(
             id = alarmInterim.id,
             title = alarmInterim.title,
-            responseTime = alarmInterim.responseTime,
+            hour = alarmInterim.hour,
+            minute = alarmInterim.minute,
             responseTimeMillis = alarmInterim.responseTimeMillis,
             intentUri = alarmInterim.intentUri,
-            requestCodes = mapRequestCodesToJson(alarmInterim.requestCodes),
-            daysTriggerBlob = mapDaysTriggerToJson(alarmInterim.daysTrigger),
+            oneTimeRequestCode = alarmInterim.oneTimeRequestCode,
+            daysTriggerJson = mapDaysTriggerToJson(alarmInterim.daysTrigger),
             isEnabled = alarmInterim.isEnabled,
             isRepeated = alarmInterim.isRepeated
         )
@@ -55,37 +57,26 @@ class AlarmClockRepositoryImpl(private val alarmDao : IAlarmClockDao) : IAlarmCl
         return AlarmInterim(
             id = alarmClock.id,
             title = alarmClock.title,
-            responseTime = alarmClock.responseTime,
+            hour = alarmClock.hour,
+            minute = alarmClock.minute,
             responseTimeMillis = alarmClock.responseTimeMillis,
             intentUri = alarmClock.intentUri,
-            requestCodes = mapRequestCodesJsonToArray(alarmClock.requestCodes),
-            daysTrigger = mapDaysTriggerJsonToArray(alarmClock.daysTriggerBlob),
+            oneTimeRequestCode = alarmClock.oneTimeRequestCode,
+            daysTrigger = mapDaysTriggerJsonToArray(alarmClock.daysTriggerJson),
             isEnabled = alarmClock.isEnabled,
             isRepeated = alarmClock.isRepeated
         )
     }
 
-    private fun mapDaysTriggerToJson(daysTrigger : List<Int>) : String {
+    private fun mapDaysTriggerToJson(daysTrigger : List<Day>) : String {
         val gson = GsonBuilder().setPrettyPrinting().create()
         return gson.toJson(daysTrigger)
     }
 
-    private fun mapDaysTriggerJsonToArray(json : String) : List<Int>{
+    private fun mapDaysTriggerJsonToArray(json : String) : List<Day>{
         val gson = GsonBuilder().setPrettyPrinting().create()
-        val type : Type = object : TypeToken<List<Int>>() {}.type
+        val type : Type = object : TypeToken<List<Day>>() {}.type
         return gson.fromJson(json, type)
     }
-
-    private fun mapRequestCodesToJson(requestCodes : List<Int>) : String{
-        val gson = GsonBuilder().setPrettyPrinting().create()
-        return gson.toJson(requestCodes)
-    }
-
-    private fun mapRequestCodesJsonToArray(json : String) : List<Int>{
-        val gson = GsonBuilder().setPrettyPrinting().create()
-        val type : Type = object : TypeToken<List<Int>>(){}.type
-        return gson.fromJson(json, type)
-    }
-
 
 }
