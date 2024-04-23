5d39ea7072864774e25bb8293e1e063e5468bd91
msoup <m.soup2016@yandex.ru>
2024-04-23 16:40:00 +0300

    - Added update method for future updates - Changed return type of getAllAlarmClocks method from List<> to Flow<>

--- app/src/main/java/com/project/alarmwakeup/data/alarm_clock/IAlarmClockDao.kt
+++ app/src/main/java/com/project/alarmwakeup/data/alarm_clock/IAlarmClockDao.kt
@@ -2,21 +2,26 @@ package com.project.alarmwakeup.data.alarm_clock
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.project.alarmwakeup.data.alarm_clock.models.AlarmClock
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface IAlarmClockDao {
 
-    @Insert(entity = AlarmClock::class)
-    fun insertNewAlarmClock(alarmClock: AlarmClock) : Long
-
-    @Query("SELECT * FROM AlarmClocks")
-    fun getAllAlarmClocks() : List<AlarmClock>
+    @Insert(entity = AlarmClock::class, onConflict = OnConflictStrategy.IGNORE)
+    fun insertAlarmClock(alarmClock: AlarmClock) : Long
+    @Update(entity = AlarmClock::class)
+    fun updateAlarmClock(alarmClock: AlarmClock)
 
     @Query("DELETE FROM AlarmClocks WHERE id = :alarmClockId")
     fun deleteAlarmClock(alarmClockId: Int)
 
     @Query("UPDATE AlarmClocks SET IsEnabled = :isEnabled WHERE id = :alarmClockId")
     fun updateEnablingAlarmClock(alarmClockId: Int, isEnabled : Boolean)
+
+    @Query("SELECT * FROM AlarmClocks")
+    fun getAllAlarmClocks() : Flow<List<AlarmClock>>
 }
