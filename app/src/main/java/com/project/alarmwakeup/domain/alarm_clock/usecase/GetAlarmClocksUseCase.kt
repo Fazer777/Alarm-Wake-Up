5a8121c2c30316c124150088774be10cfd9ecdb0
msoup <m.soup2016@yandex.ru>
2024-04-23 16:37:49 +0300

    - Changed name of method - Changed return type from List<> to Flow<>

--- app/src/main/java/com/project/alarmwakeup/domain/alarm_clock/usecase/GetAlarmClocksUseCase.kt
+++ app/src/main/java/com/project/alarmwakeup/domain/alarm_clock/usecase/GetAlarmClocksUseCase.kt
@@ -2,9 +2,10 @@ package com.project.alarmwakeup.domain.alarm_clock.usecase
 
 import com.project.alarmwakeup.domain.alarm_clock.IAlarmClockRepository
 import com.project.alarmwakeup.domain.alarm_clock.models.AlarmInterim
+import kotlinx.coroutines.flow.Flow
 
 class GetAlarmClocksUseCase(private val alarmRepository: IAlarmClockRepository) {
-    suspend fun execute() : List<AlarmInterim>{
-        return alarmRepository.getAllAlarmClocksFromDb()
+    fun execute() : Flow<List<AlarmInterim>>{
+        return alarmRepository.getAllAlarmClocks()
     }
 }
