c73f92c19e0a33381cd5b99c142a6a485af25eba
msoup <m.soup2016@yandex.ru>
2024-05-09 16:13:54 +0300

    - I have completed AlarmHelper, now I will build on it in further development

--- app/src/main/java/com/project/alarmwakeup/presentation/helpers/AlarmHelper.kt
+++ app/src/main/java/com/project/alarmwakeup/presentation/helpers/AlarmHelper.kt
@@ -4,9 +4,11 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.icu.util.Calendar
 import android.os.Build
 import com.project.alarmwakeup.R
+import com.project.alarmwakeup.domain.alarm_clock.models.AlarmInterim
 import com.project.alarmwakeup.domain.alarm_clock.models.Day
 import com.project.alarmwakeup.presentation.activities.AlarmActivity
 import com.project.alarmwakeup.presentation.helpers.models.BaseAlarmData
@@ -21,62 +23,122 @@ class AlarmHelper(
     private val appContext: Context,
     private val alarmManager: AlarmManager,
     private val targetCalendar: Calendar,
-    private var lastRequestCode : Int
+
     ) {
 
-    fun scheduleOneTimeAlarm(alarmData: OneTimeAlarmData): Int {
-        alarmData.setRequestCode(lastRequestCode)
-        setOneTimeAlarm(alarmData = alarmData)
-        return lastRequestCode
+    private val sharedPreferences: SharedPreferences
+    private var lastRequestCode: Int = -1
+
+    init {
+        sharedPreferences = appContext.getSharedPreferences(
+            appContext.resources.getString(R.string.shared_pref_name),
+            Context.MODE_PRIVATE
+        )
     }
 
-    fun scheduleRepeatedAlarm(alarmData: RepeatedAlarmData): ResultDataRepeatedAlarm {
+
+    private fun getValueFromSharedPref() {
+        lastRequestCode =
+            sharedPreferences.getInt(appContext.resources.getString(R.string.request_code_name), 0)
+    }
+
+    private fun saveValueToSharedPreferences() {
+        sharedPreferences.edit()
+            .putInt(appContext.resources.getString(R.string.request_code_name), lastRequestCode)
+            .apply()
+    }
+
+    fun scheduleOneTimeAlarm(alarmData: OneTimeAlarmData): AlarmInterim {
+        getValueFromSharedPref()
+        alarmData.setRequestCode(lastRequestCode)
+        setOneTimeAlarm(alarmData = alarmData)
+        saveValueToSharedPreferences()
+        return createOneTimeAlarmObject(alarmData)
+    }
+
+
+    fun scheduleRepeatedAlarm(alarmData: RepeatedAlarmData) : AlarmInterim {
+        getValueFromSharedPref()
         setRepeatedAlarm(alarmData = alarmData)
-        return ResultDataRepeatedAlarm(lastRequestCode, alarmData.daysTrigger)
+        saveValueToSharedPreferences()
+        return createRepeatedAlarmObject(alarmData)
     }
 
     // TODO ("Need to do canceling alarm")
     fun cancelAlarm() {
 
     }
+    private fun createRepeatedAlarmObject(alarmData: RepeatedAlarmData): AlarmInterim {
+
+        return AlarmInterim(
+            id = 0,
+            title = alarmData.title,
+            hour = alarmData.hour,
+            minute = alarmData.minute,
+            responseTimeMillis = 0,
+            intentUri = "empty",
+            oneTimeRequestCode = null ,
+            daysTrigger = alarmData.daysTrigger,
+            isEnabled = true,
+            isRepeated = true
+        )
+    }
+
+    private fun createOneTimeAlarmObject(alarmData: OneTimeAlarmData) : AlarmInterim{
+        return AlarmInterim(
+            id = 0,
+            title = alarmData.title,
+            hour = alarmData.hour,
+            minute = alarmData.minute,
+            responseTimeMillis = 0,
+            intentUri = "empty",
+            oneTimeRequestCode = lastRequestCode - 1 ,
+            daysTrigger = listOf(),
+            isEnabled = true,
+            isRepeated = false
+        )
+    }
+
+
 
     private fun setRepeatedAlarm(alarmData: RepeatedAlarmData) {
         for (i in 0 until 7) {
             when (targetCalendar.get(Calendar.DAY_OF_WEEK)) {
                 Calendar.MONDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[0], alarmData)
                 }
 
                 Calendar.TUESDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[1], alarmData)
                 }
 
                 Calendar.WEDNESDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[2], alarmData)
                 }
 
                 Calendar.THURSDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[3], alarmData)
                 }
 
                 Calendar.FRIDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[4], alarmData)
                 }
 
                 Calendar.SATURDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[5], alarmData)
                 }
 
                 Calendar.SUNDAY -> {
-                    setAlarmForDay(alarmData.daysTrigger[0] , alarmData)
+                    setAlarmForDay(alarmData.daysTrigger[6], alarmData)
                 }
             }
+            //lastRequestCode++
             targetCalendar.add(Calendar.DAY_OF_MONTH, 1)
         }
     }
 
-    private fun setAlarmForDay(day: Day, alarmData: BaseAlarmData){
-        if (!day.isEnabled){
+    private fun setAlarmForDay(day: Day, alarmData: BaseAlarmData) {
+        if (!day.isEnabled) {
             return
         }
         checkRescheduleForNDays(amountDays = 7)
@@ -89,29 +151,10 @@ class AlarmHelper(
         setAlarm(alarmData = alarmData)
     }
 
-//    fun getOneTimeAlarmIntent(alarmData: OneTimeAlarmData): Intent {
-//        val alarmIntent = Intent(appContext, AlarmActivity::class.java).apply {
-//            putExtra(
-//                appContext.resources.getString(R.string.intent_key_one_time_alarm), alarmData
-//            )
-//        }
-//
-//        return alarmIntent
-//    }
-//
-//    fun getRepeatedAlarmIntent(): Intent {
-//        val alarmIntent = Intent(appContext, AlarmActivity::class.java)
-////        alarmIntent.putExtra(
-////            appContext.resources.getString(R.string.intent_key_repeating_alarm),
-////            alarm.isRepeated
-////        )
-//
-//        return alarmIntent
-//    }
 
-    fun getAlarmIntent(alarmData: BaseAlarmData) : Intent {
+    fun getAlarmIntent(alarmData: BaseAlarmData): Intent {
         val alarmIntent = Intent(appContext, AlarmActivity::class.java).apply {
-            putExtra(appContext.resources.getString(R.string.intent_key_alarm_data), alarmData )
+            putExtra(appContext.resources.getString(R.string.intent_key_alarm_data), alarmData)
         }
         return alarmIntent
     }
@@ -128,7 +171,7 @@ class AlarmHelper(
     }
 
     private fun setAlarm(alarmData: BaseAlarmData) {
-        val operation = getOperationIntent(getAlarmIntent(alarmData =  alarmData), lastRequestCode)
+        val operation = getOperationIntent(getAlarmIntent(alarmData = alarmData), lastRequestCode)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             if (alarmManager.canScheduleExactAlarms()) {
@@ -145,6 +188,10 @@ class AlarmHelper(
                 operation
             )
         }
+        increaseRequestCode()
+    }
+
+    private fun increaseRequestCode() {
         lastRequestCode++
     }
 
